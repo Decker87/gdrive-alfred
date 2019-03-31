e445fa7d36990d4747304594b3573feb1e39aeb5
Adam S <decker.adam@gmail.com>
2019-03-30 23:55:09 -0600

    CircleCI basics working.

--- package.sh
+++ package.sh
@@ -1,15 +1,19 @@
 # Determine version
 version="1.`git rev-list --count HEAD`"
 packageName="gdrive-alfred-v$version.alfredworkflow"
+packagePath="bin/$packageName"
+mkdir "bin"
 
 # Install necessary packages locally
 pip install --upgrade --target=workflow_src/pylib_dist google-api-python-client google-auth-httplib2 google-auth-oauthlib
 
 # Zip it up (unfortunately zip util requires us to pushd to avoid copying workflow_src)
-mkdir bin
 pushd workflow_src
-zip -r "../bin/$packageName" *
+zip -r "../$packagePath" *
 popd
+
+# Helpful msg
+du -sh "$packagePath"
 
 # Clean up if asked to
 if [[ "$1" == "--clean-up" ]]; then
