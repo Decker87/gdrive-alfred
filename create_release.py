8c51850bed93e1a5c08318d0b7ea8f45b15a4d26
Adam S <decker.adam@gmail.com>
2019-03-31 02:07:58 -0600

    Auto-increment version when creating release.

--- create_release.py
+++ create_release.py
@@ -1,6 +1,26 @@
 import requests
 import argparse
 import json
+import re
+
+# TODO: Comment this file
+
+def getLatestRelease(ghUser, ghToken):
+    auth = requests.auth.HTTPBasicAuth(ghUser, ghToken)
+    url = "https://api.github.com/repos/Decker87/gdrive-alfred/releases/latest"
+    r = requests.get(url, auth=auth)
+    return r.json()
+
+def getLatestReleaseVersion(ghUser, ghToken):
+    latestRelease = getLatestRelease(ghUser, ghToken)
+    return latestRelease["tag_name"]
+
+def getIncrementedVersion(ghUser, ghToken):
+    latestVersion = getLatestReleaseVersion(ghUser, ghToken)
+    m = re.match(r"(\d+)\.(\d+)\.(\d+)", latestVersion)
+    major, minor, patch = [int(x) for x in m.groups()]
+    patch += 1
+    return "%i.%i.%i" % (major, minor, patch)
 
 def createRelease(ghUser, ghToken, versionStr, name):
     print(ghUser)
@@ -20,11 +40,22 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("GITHUB_USER", help="Github username.")
     parser.add_argument("GITHUB_TOKEN", help="Personal access token for Github.")
-    parser.add_argument("VERSION", help="SemVer-style version to use.")
-    parser.add_argument("NAME", nargs='?', help="Name of the release.", default="default name")
+    parser.add_argument("--version", help="SemVer-style version to use.")
+    parser.add_argument("--name", help="Name of the release.", default="default release name")
     args = parser.parse_args()
+
+    # Parse to normal var names
+    ghUser = args.GITHUB_USER
+    ghToken = args.GITHUB_TOKEN
+    version = args.version
+    name = args.name
+
+    # If no version is given, use the latest + 1
+    if not version:
+        version = getIncrementedVersion(ghUser, ghToken)
     
-    createRelease(args.GITHUB_USER, args.GITHUB_TOKEN, args.VERSION, args.NAME)
+    # Do it!
+    createRelease(ghUser, ghToken, version, name)
 
 if __name__ == "__main__":
     main()
