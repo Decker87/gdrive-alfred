3d6361ac8180359701bb7bfb817408291242f29d
Adam S <decker.adam@gmail.com>
2019-04-07 00:35:18 -0600

    Auto-release and asset upload working from CircleCI.

--- ci/release_if_new_version.sh
+++ ci/release_if_new_version.sh
@@ -1,10 +1,11 @@
 versionPath="workflow_src/VERSION.txt"
+packagePath="bin/gdrive-alfred.alfredworkflow"
 
 # If the version increased in this commit, then roll a new version!
 if ! git diff --exit-code HEAD^ HEAD "$versionPath"; then
     newVersion="$(cat $versionPath)"
     echo "newVersion=$newVersion"
-    python ci/create_release.py $GITHUB_USER $GITHUB_TOKEN $newVersion
+    python ci/create_release.py --asset-path "$packagePath" $GITHUB_USER $GITHUB_TOKEN $newVersion
 else
     echo "$versionPath not updated this commit."
 fi
