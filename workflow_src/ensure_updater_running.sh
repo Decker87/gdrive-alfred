cc1426c99d5b6406336b0cc048862eacfc5ea1d6
Adam Schirmacher <adam.schirmacher@gusto.com>
2019-04-10 17:05:07 -0600

    Improve file naming.

--- workflow_src/ensure_updater_running.sh
+++
@@ -1,4 +0,0 @@
-# This script makes sure at least 1 (preferably just 1) copy of local_cache_updater.py is running in the background.
-if ! ps -x | grep -v grep | grep -qF "python local_cache_updater.py"; then
-  nohup python "local_cache_updater.py" &
-fi
