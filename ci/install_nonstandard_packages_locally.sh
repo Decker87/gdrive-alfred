d0273b29e780bc07b0910cf287601de19b5fb772
Adam Schirmacher <adam.schirmacher@gusto.com>
2019-04-12 13:26:23 -0600

    Init the google module to allow imports.

--- ci/install_nonstandard_packages_locally.sh
+++ ci/install_nonstandard_packages_locally.sh
@@ -1,2 +1,5 @@
-target="$0"
+target="$1"
 pip install --upgrade --target="$target" google-api-python-client google-auth-httplib2 google-auth-oauthlib requests
+
+# For some reason, the "google" library doesn't have an __init__.py which prevents it from being imported.
+touch "$target/google/__init__.py"
--- tests/e2e.sh
+++ tests/e2e.sh
@@ -1,7 +1,7 @@
 # Meant to be run from base dir
 cd "workflow_src"
-ci/install_nonstandard_packages_locally.sh "pylib_dist"
-rm cache.json cache.pickle
+../ci/install_nonstandard_packages_locally.sh "pylib_dist"
+rm cache.json 2>/dev/null
 python continuously_update_local_cache.py --spoof-server --debug
 python search_local_cache.py "Edward" | tee out.txt
 grep -Fq "Org Chart" out.txt
