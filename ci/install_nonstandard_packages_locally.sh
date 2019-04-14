8f22ad6f423e696d3dd9351a57ff74aab0d14fab
Adam Schirmacher <adam.schirmacher@gusto.com>
2019-04-13 18:34:58 -0600

    Upgrade pip on CircleCI runs.

--- ci/install_nonstandard_packages_locally.sh
+++ ci/install_nonstandard_packages_locally.sh
@@ -1,3 +1,8 @@
+# CircleCI images have an old pip for some reason
+if [[ "$CIRCLECI" == "true" ]]; then
+    sudo pip install --upgrade pip
+fi
+
 target="$1"
 pip install --target="$target" google-api-python-client google-auth-httplib2 google-auth-oauthlib requests
 pip install --target="$target" --no-deps --platform=macosx-10.13-intel ujson
