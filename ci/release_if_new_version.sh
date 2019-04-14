30a5ebedd1bb487fa70a0d4fadd8f87ab9b21c02
Adam Schirmacher <adam.schirmacher@gusto.com>
2019-04-13 18:58:44 -0600

    Only create GH release from master branch.

--- ci/release_if_new_version.sh
+++ ci/release_if_new_version.sh
@@ -1,6 +1,11 @@
 versionPath="workflow_src/VERSION.txt"
 packagePath="bin/gdrive-alfred.alfredworkflow"
 
+# If we aren't on master branch from within CircleCI, don't do anything.
+if [[ "$CIRCLE_BRANCH" != "master" ]]; then
+    exit
+fi
+
 # If the version increased in this commit, then roll a new version!
 if ! git diff --exit-code HEAD^ HEAD "$versionPath"; then
     newVersion="$(cat $versionPath)"
