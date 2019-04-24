92930dc6f9140fc46037d5dc3d99f232ea0b653f
Adam Schirmacher <adam.schirmacher@gusto.com>
2019-04-24 10:03:28 -0600

    Clean up main so search only searches.

--- workflow_src/main.py
+++ workflow_src/main.py
@@ -13,24 +13,8 @@ MAX_ALFRED_ITEMS = 20
 sys.path.insert(0, "pylib_dist")
 
 def action_search(query):
-    fullAlfredItemList = []
-    searchAlfredItems = search.search(query)
-    workflowUpdateAlfredItem = getWorkflowUpdateAlfredItem()
-
-    if workflowUpdateAlfredItem:
-        fullAlfredItemList.append(workflowUpdateAlfredItem)
-
-    if searchAlfredItems:
-        fullAlfredItemList += searchAlfredItems[:MAX_ALFRED_ITEMS]
-
-    # Add UUIDs so we can record easily what was chosen when
-    for item in fullAlfredItemList:
-        if "arg" in item:
-            item["arg"] = "%s|%s" % (uuid.uuid4().hex, item["arg"])
-
-    # Print for Alfred
-    # See https://www.alfredapp.com/help/workflows/inputs/script-filter/json/ for format
-    print(json.dumps({"items": fullAlfredItemList}, indent = 4))
+    """Returns a list of alfred items."""
+    return search.search(query)
 
 def main():
     parser = argparse.ArgumentParser()
@@ -38,8 +22,24 @@ def main():
     parser.add_argument("--query", help="Query if searching")
     args = parser.parse_args()
 
+    # Build item list from actions
+    items = []
     if args.action == "search" and args.query != None:
-        return action_search(args.query)
+        items += action_search(args.query)
+
+    # Add Update item at the top
+    workflowUpdateAlfredItem = getWorkflowUpdateAlfredItem()
+    if workflowUpdateAlfredItem:
+        items.insert(0, workflowUpdateAlfredItem)
+
+    # Add UUIDs so we can record easily what was chosen when
+    for item in items:
+        if "arg" in item:
+            item["arg"] = "%s|%s" % (uuid.uuid4().hex, item["arg"])
+
+    # Print for Alfred
+    # See https://www.alfredapp.com/help/workflows/inputs/script-filter/json/ for format
+    print(json.dumps({"items": items}, indent = 4))
 
 if __name__ == '__main__':
     main()
