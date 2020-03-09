fd0ef0eb73806e2f70d44e264d6cd0e6d8db949b
Adam Schirmacher <decker.adam@gmail.com>
2020-03-09 15:46:50 -0600

    Re-index e2e tests.

--- tests/e2e.sh
+++ tests/e2e.sh
@@ -15,7 +15,7 @@ rm cache.json 2>/dev/null
 python continuously_update_local_cache.py --spoof-server --debug
 python main.py --query "Kelly sheet" | tee out.txt
 if ! grep -Fq "1tgzWrjCGkJLjTPlQO3Uuz42XJKqH0Mu13VUkVAGBt1c" out.txt; then
-    exit 1
+    exit 2
 fi
 
 echo "TEST: basic search with cache, doc type"
@@ -23,13 +23,13 @@ rm cache.json 2>/dev/null
 python continuously_update_local_cache.py --spoof-server --debug
 python main.py --query "Kelly doc" | tee out.txt
 if ! grep -Fq "1uwEPYqv1Vl9dsAs3X51qrUigE2NfExOBT764XyaFifs" out.txt; then
-    exit 1
+    exit 3
 fi
 
 echo "TEST: Junk query should have empty item results"
 python main.py --query "fajksdhfhadslfjhdsafljkadh" | tee out.txt
 if ! grep -Fq '"items": []' out.txt; then
-    exit 2
+    exit 4
 fi
 
 echo "TEST: If version is out of date, should add an item to update"
@@ -37,7 +37,7 @@ python continuously_get_latest_workflow.py --debug --spoof-newer-version
 python main.py --query "Edward" | tee out.txt
 if ! grep -Fq "An update is available" out.txt; then
     rm "latest/VERSION.txt"
-    exit 3
+    exit 5
 fi
 rm "latest/VERSION.txt"
 
@@ -46,11 +46,11 @@ rm cache.json 2>/dev/null
 python main.py --query "Edward" | tee out.txt
 # Make sure it has an item to update the cache
 if ! grep -Fq "Updating cache" out.txt; then
-    exit 4
+    exit 6
 fi
 # Make sure that's the only item
 t=`cat out.txt | grep '"title"' | wc -l`
 t=`echo $t`
 if [[ "$t" != "1" ]]; then
-    exit 5
+    exit 7
 fi
